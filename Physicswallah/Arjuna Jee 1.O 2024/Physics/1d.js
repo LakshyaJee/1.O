e0ceddee5a33f4956d0ddf4dfff78cfa3ecdd64a
LakshyaJee <147268432+LakshyaJee@users.noreply.github.com>
2024-06-02 17:26:13 +0530

    changes

---
+++ Physicswallah/Arjuna Jee 1.O 2024/Physics/1d.js
@@ -0,0 +1,106 @@
+[
+    {
+      "lectureNumber": 1,
+      "lectureName": "Motion in a Straight line 12 : Questi...",
+      "lectureCode": "9deaf624-c0db-42fe-baeb-c31f62f10ab0",
+      "classNotesCode": "910e2be8-99cc-4f8c-8f9f-8d486fd5e597.pdf",
+      "dppsPdfCode": "49c93b0a-ea1e-43fe-96b0-a917a83a28fa.pdf",
+      "dppsSolutionCode": "8b94f966-7d27-44bf-a146-d32e0c06adc5"
+    },
+    {
+      "lectureNumber": 2,
+      "lectureName": "Motion in a Straight line 11 : Questi...",
+      "lectureCode": "0bb75c87-a8b1-4d41-af77-6afac8549e8c",
+      "classNotesCode": "8d2e4d79-9042-4f7a-98ca-f522ffa8de4d.pdf",
+      "dppsPdfCode": "f81f37b2-481a-4359-b5c0-aff5a9d26a2a.pdf",
+      "dppsSolutionCode": "51ec5671-fe78-4804-ad9c-6aa7553802a8"
+    },
+    {
+      "lectureNumber": 3,
+      "lectureName": "Motion in a Straight line 10 : Graph ...",
+      "lectureCode": "9994c5b0-44be-4e3a-b1ca-cb614095860e",
+      "classNotesCode": "b10c0b61-a45f-4e1a-abe7-068b15c79fb4.pdf",
+      "dppsPdfCode": "a0d9f2c1-9839-4d8c-b03d-93f6dcdad6b9.pdf",
+      "dppsSolutionCode": "d6c2b353-27f2-47c7-ae33-a306cce31b07"
+    },
+    {
+      "lectureNumber": 4,
+      "lectureName": "Motion in a Straight line 09 : Integr...",
+      "lectureCode": "efda8d91-5b52-4354-b2eb-c7b31935b722",
+      "classNotesCode": "248abcc2-6d0a-4a3f-a001-6f3256122e5b.pdf",
+      "dppsPdfCode": "bb1327af-dc98-44e4-901e-ee5d4174ca8b.pdf",
+      "dppsSolutionCode": "3dda97cb-4e4a-4189-9afe-702c2b10b767"
+    },
+    {
+      "lectureNumber": 5,
+      "lectureName": "Motion in a Straight line 08 : Graph ...",
+      "lectureCode": "b279dddb-25f7-41f2-aa5f-286bb43d3035",
+      "classNotesCode": "e51922d3-b6f5-4f29-98a7-c3aadc51f291.pdf",
+      "dppsPdfCode": "b70237db-995f-402a-9c03-24ff82fb14ad.pdf",
+      "dppsSolutionCode": "fa526ac4-3cc1-47c8-b9c1-0b7152fc69f6"
+    },
+    {
+      "lectureNumber": 6,
+      "lectureName": "Motion in a Straight line 07 : Graph",
+      "lectureCode": "c0f0d988-443c-42e6-9bf3-cc7711c227c3",
+      "classNotesCode": "6991137d-ce63-46e7-9f9a-f6f241ebf532.pdf",
+      "dppsPdfCode": "eb0bd5a0-65e9-4c7e-b30b-3513efb5a875.pdf",
+      "dppsSolutionCode": "91be0b09-7b7e-4894-8f11-5ca6ac728c87"
+    },
+    {
+      "lectureNumber": 7,
+      "lectureName": "Motion in a Straight line 06 : Averag...",
+      "lectureCode": "a33c8202-5c54-4851-b563-cdd12fe55bdb",
+      "classNotesCode": "e7546497-1b73-44a9-b858-9129e24932c4.pdf",
+      "dppsPdfCode": "f9aabfa4-c02b-40a9-bfde-6e3e366e2e08.pdf",
+      "dppsSolutionCode": "11078e92-0f3e-40cf-86ce-ec6c4375fa3d"
+    },
+    {
+      "lectureNumber": 8,
+      "lectureName": "Motion in a Straight line 05 : Differ...",
+      "lectureCode": "c6ce3680-7ed1-4868-b127-ce5091feee42",
+      "classNotesCode": "9c259bfa-2aa0-43a5-8680-73aee2e11477.pdf",
+      "dppsPdfCode": "6ba85bb2-a81f-4d38-bec6-25e4eaa3a5f2.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 9,
+      "lectureName": "Motion in a Straight line 04 : Differ...",
+      "lectureCode": "6ba18198-355d-473c-bbe8-b7faa1c6e9ca",
+      "classNotesCode": "899cbb18-20cc-489b-8105-f0761a6aa92e.pdf",
+      "dppsPdfCode": "1376d0ac-fe8f-4d94-8a5b-87232c84f0d6.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 10,
+      "lectureName": "Motion in a Straight line 03 : Slope ...",
+      "lectureCode": "5907dcb7-27fb-472c-b2a7-3eecd65df2ed",
+      "classNotesCode": "f02c08d4-fd81-4b7e-a16a-2290e8d98815.pdf",
+      "dppsPdfCode": "34c74878-8134-4d5e-8ced-f104798d1ed9.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 11,
+      "lectureName": "Motion in a Straight line 02 :  Avera...",
+      "lectureCode": "bbcf31eb-743d-4909-aec0-ff25c8e56894",
+      "classNotesCode": "bf06637a-dc6c-48a2-b0a9-0e13ddac80d4.pdf",
+      "dppsPdfCode": "6d2928a5-a112-4ac9-b395-161a8ad29270.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 12,
+      "lectureName": "Motion in a Straight line 01 : Motion...",
+      "lectureCode": "f73e4885-5c66-4086-b06a-730a418a1fe9",
+      "classNotesCode": "98add40f-564e-42fb-a59c-390bfaac3c64.pdf",
+      "dppsPdfCode": "d087cf83-a73d-44ef-97e0-f51efd60cc3d.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 13,
+      "lectureName": "Motion in a Straight line 09 : Integr...",
+      "lectureCode": "53cab60a-11b9-402d-b444-a4a2246e4de7",
+      "classNotesCode": "910e2be8-99cc-4f8c-8f9f-8d486fd5e597.pdf",
+      "dppsPdfCode": "0b62d773-1a32-46c9-ac12-022b6408f929.pdf",
+      "dppsSolutionCode": ""
+    }
+  ]
---
+++ Physicswallah/Arjuna Jee 1.O 2024/Physics/nlm.js
@@ -0,0 +1,108 @@
+[
+    {
+      "lectureNumber": 1,
+      "lectureName": "Newton law of Motion 12 : Idea of gef...",
+      "lectureCode": "f239dbf3-3c90-4b82-994a-ae65434770fa",
+      "classNotesCode": "c411508c-72f8-4a87-8735-c06338b93542.pdf",
+      "dppsPdfCode": "7804f96b-dfee-40eb-b6bf-13a6d029d803.pdf",
+      "dppsSolutionCode": "058cfcd9-d195-45ea-8dcb-c667aed81c65"
+    },
+    {
+      "lectureNumber": 2,
+      "lectureName": "Newton law of Motion 11 : Pseudo Forc...",
+      "lectureCode": "cc15e8c0-777c-4496-8ae7-1685abf000ff",
+      "classNotesCode": "16565919-95a9-4a42-86e8-9006b594456a.pdf",
+      "dppsPdfCode": "818348d6-ab3b-4df0-96bc-5bdf8bb7b03b.pdf",
+      "dppsSolutionCode": "48f72b72-043a-4495-aac3-88a3bf23ace8"
+    },
+    {
+      "lectureNumber": 3,
+      "lectureName": "Newton law of Motion 10 : Constraint ...",
+      "lectureCode": "2e7c1da2-7032-4628-bd28-138962393c19",
+      "classNotesCode": "da5976c2-a91c-4ad9-ad24-f2797856e840.pdf",
+      "dppsPdfCode": "fd31ab53-07d1-4510-952e-2e89487e225e.pdf",
+      "dppsSolutionCode": "503f46cb-7326-43a1-9d58-e562fd127a22"
+    },
+    {
+      "lectureNumber": 4,
+      "lectureName": "Newton law of Motion 09 : Spring Cons...",
+      "lectureCode": "de45e18c-c09d-4cd1-bc80-62f4faf9cd30",
+      "classNotesCode": "2037c518-3158-4682-ba17-f4277937a6da.pdf",
+      "dppsPdfCode": "b7cd69d0-fc9e-41bb-97bf-4d4416310351.pdf",
+      "dppsSolutionCode": "4eb279a7-bad6-4725-a473-b52dde63e75f"
+    },
+    {
+      "lectureNumber": 5,
+      "lectureName": "Newton law of Motion 08 (Part 02) : Q...",
+      "lectureCode": "a44fa02e-5521-4eaf-b1db-abe0980d6362",
+      "classNotesCode": "f8d3c78a-ff11-43c3-8f3c-33374aa3897f.pdf",
+      "dppsPdfCode": "2d8ed95d-932b-4942-8ef5-dffb13dbe905.pdf",
+      "dppsSolutionCode": "e11e94de-a1f2-4b93-b79a-e1a69047a96c"
+    },
+    {
+      "lectureNumber": 6,
+      "lectureName": "Newton law of Motion 08 : Important Q...",
+      "lectureCode": "02b9fc2c-f29e-46e4-bc39-fbf303de4749",
+      "classNotesCode": "4f91b577-1850-40e6-86b6-0729419337b4.pdf",
+      "dppsPdfCode": "d817e545-6a36-4e4a-ac21-02bed7b96801.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 7,
+      "lectureName": "Newton law of Motion 07 : Question ba...",
+      "lectureCode": "83dc62dd-986c-4b57-80be-2d1be0c6b9d8",
+      "classNotesCode": "a3e70cb1-7882-4b31-97f9-b5196fb7897d.pdf",
+      "dppsPdfCode": "4e6cad9b-49ca-4e59-8432-daee069671e3.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 8,
+      "lectureName": "Newton law of Motion 06 : Question ba...",
+      "lectureCode": "ed03e4f8-630a-42fb-b96e-dcb571525009",
+      "classNotesCode": "e809f911-544c-4fa5-86a4-3e3652e63d78.pdf",
+      "dppsPdfCode": "2019083d-5f2d-4d8f-b478-ed7530824664.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 9,
+      "lectureName": "Newton law of Motion 05 : Question ba...",
+      "lectureCode": "8123c1ca-2672-47b8-ba53-a3689975cdb9",
+      "classNotesCode": "c7479964-fd2c-468d-bd89-61f856082954.pdf",
+      "dppsPdfCode": "5771c065-a86f-4912-97b1-b7175906e78f.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 10,
+      "lectureName": "Newton law of Motion 04 : Question ba...",
+      "lectureCode": "be84ea7e-1bd4-42a8-bb00-b9bc6a69a88d",
+      "classNotesCode": "aa33417b-c63b-4615-9468-48dda467bdf4.pdf",
+      "dppsPdfCode": "273e85a8-a72a-4de7-b4af-04c8be7a94e7.pdf",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 11,
+      "lectureName": "Newton law of Motion 03 : F.B.D || Qu...",
+      "lectureCode": "ccc1fb54-1d67-439f-a6b3-b9f517eac763",
+      "classNotesCode": "cf6edfca-2ead-498c-ac2a-b4da6be06e8f.pdf",
+      "dppsPdfCode": "",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 12,
+      "lectureName": "Newton law of Motion 02 : Newton's la...",
+      "lectureCode": "c64d582e-70c6-4e5a-83db-202ee565816d",
+      "classNotesCode": "f9d41ae1-ec4a-48fb-ade5-3885a99ae316.pdf",
+      "dppsPdfCode": "",
+      "dppsSolutionCode": ""
+    },
+    {
+      "lectureNumber": 13,
+      "lectureName": "Newton law of Motion 01 : Newton law ...",
+      "lectureCode": "bf7f1601-a424-4ff3-96b7-cad7f423eb7b",
+      "classNotesCode": "fac7cf1a-3802-4592-8aae-a62e47ca3cc0.pdf",
+      "dppsPdfCode": "",
+      "dppsSolutionCode": ""
+    }
+  ] 
+  
+  ​
