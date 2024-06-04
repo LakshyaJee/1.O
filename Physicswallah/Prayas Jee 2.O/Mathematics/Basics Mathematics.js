b339e0d629efbc155dd375fe6fbb763abdcb5217
LakshyaJee <147268432+LakshyaJee@users.noreply.github.com>
2024-06-04 06:27:10 +0530

    changes

---
+++ Physicswallah/Prayas Jee 2.O/Mathematics/Basics Mathematics.js
@@ -0,0 +1,60 @@
+
+const lectureData= [
+{
+    "lectureNumber": 1,
+    "lectureName": "Binomial Theorem 01 : Pascal's Triangle || Binomial Expansion || Question Practice || (No DPP)",
+    "lectureCode": "b9c808ef-43b4-4614-a2bb-a390ef74701d",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  },
+  {
+    "lectureNumber": 2,
+    "lectureName": "Binomial Theorem 02 : Number of Rational Irrational Terms || Finding Coefficients || Question Practice",
+    "lectureCode": "10290d8e-9a3d-484f-acf9-808d36b3c506",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  },
+  {
+    "lectureNumber": 3,
+    "lectureName": "Binomial Theorem 03 : Finding Coefficients || Numerically Greatest Term || Properties of Binomial Coefficients || Question Practice",
+    "lectureCode": "539c64ee-81ed-4e30-9c58-3728bc4196b8",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  },
+  {
+    "lectureNumber": 4,
+    "lectureName": "Binomial Theorem 04 : Properties of Binomial Coefficients || Series || (No DPP)",
+    "lectureCode": "34327a4e-8228-4b2b-890c-b765c8baeb94",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  },
+  {
+    "lectureNumber": 5,
+    "lectureName": "Binomial Theorem 05 : Different Types of Series || Overpowered Question Practice",
+    "lectureCode": "22cbc284-c15b-4aaf-8046-be49a73d6df5",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  },
+  {
+    "lectureNumber": 6,
+    "lectureName": "Binomial Theorem 06 : Different types of Series involving Binomial Coefficients || OP-QP",
+    "lectureCode": "dc9dbd9d-07f9-457c-be4d-d803467b5d8b",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  },
+  {
+    "lectureNumber": 7,
+    "lectureName": "Binomial Theorem 07 : Questions Involving Double Sigma || Multinomial Theorem || OP-QP",
+    "lectureCode": "c6ce31be-32d7-4afa-a8a4-b3899efeda8e",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  },
+  {
+    "lectureNumber": 8,
+    "lectureName": "Binomial Theorem 08 : Multinomial Theorem || Finding Remainder || Last Digits || Integral & Fractional Part",
+    "lectureCode": "fdeeaaea-bed3-4dd1-a9f5-ba65b0d725d2",
+    "classNotesCode": "",
+    "dppsPdfCode": ""
+  }
+];
+
